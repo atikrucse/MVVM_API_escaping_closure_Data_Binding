a759784d7e73e958d23f0a178cd02fd6be93fb68
Mynul Atik <53344044+atikrucse@users.noreply.github.com>
2023-09-10 21:52:29 +0600

    First Commit

---
+++ MVVM_@escaping_closure_Data Binding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_@escaping_closure_Data Binding
+//
+//  Created by Atik  on 10/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
